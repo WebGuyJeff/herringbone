21fbab986ccf17a8a740f6bb07531fb42db2c6b3
Jefferson <me@jeffersonreal.com>
2021-09-23 10:57:52 +0100

    hidey header js simplified

--- js/hb_hideheader.js
+++ js/hb_hideheader.js
@@ -12,69 +12,49 @@
 gsap.registerPlugin( CSSRulePlugin );
 
 var hb_header = (function() {
-	let dims = {};
 
 	//Get the elements being animated
-	let header = document.querySelector('.header');
-	let welc = document.querySelector('.welcome');
+	let header = document.querySelector( '.header' );
 
 	//This function calculates the new position values for the animation
-	function getDims(toggle) {
+	function getHeaderHeight() {
 
-		// Get the header dims
-		let headerFontSize = window.getComputedStyle(header).fontSize;
-		let headerHeight = window.getComputedStyle(header).height;
-		let headerMargTop = window.getComputedStyle(header).marginTop;
-		let headerMargBot = window.getComputedStyle(header).marginBottom;
+		// Get the header height
+		let headerFontSize = window.getComputedStyle( header ).fontSize;
+		let headerHeight = parseFloat( window.getComputedStyle( header ).height );
 
-		//Combine the height and margins for the absolute height
-		let headerAbsHeight = parseFloat(headerHeight)
-							+ parseFloat(headerMargTop)
-							+ parseFloat(headerMargBot);
+		// We use the element font size to convert the values to em units
+		let headerHeightEm = ( parseFloat( headerHeight ) / parseFloat( headerFontSize )).toFixed(2);
 
-		// Get welcome section margin top
-		let welcFontSize = window.getComputedStyle(welc).fontSize;
-		let welcMarg = window.getComputedStyle(welc).marginTop;
-
-		// We use the element font size to get the current value of 1em in px
-		// to account for fluid CSS units where 1em may not equal 16px.
-		headerAbsHeight = (parseFloat(headerAbsHeight) / parseFloat(headerFontSize)).toFixed(2);
-		welcMarg = (parseFloat(welcMarg) / parseFloat(welcFontSize)).toFixed(2);
-
-		return {
-			y: headerAbsHeight,
-			marg: welcMarg,
-		}
+		return headerHeightEm;
 	}
 
-	let idle = true;
-	let toggle = false;
-	let move;
-	let offset;
-	let marg;
+	let isAnimating = false;
+	let isToggled = false;
+	let newMargTop;
+	let btnOffset;
+	let heightEm;
 
 	return {
 		toggle: function() {
 
 			// Check if the header is already toggled and supply vars to invert the state
 			// We update variables every toggle as layout may have changed
-			if (idle) {
-				idle = false;
-				if (toggle) {
-					toggle = false;
+			if ( !isAnimating ) {
+				isAnimating = true;
+				if ( isToggled ) {
+					isToggled = false;
 					bodyClassToggle();
-					dims = getDims(toggle);
-					move = -Math.abs(dims.y) + 'em';
-					offset = dims.y + 'em';
-					marg = (parseFloat(dims.marg) - parseFloat(dims.y)).toFixed(2) + 'em';
+					heightEm       = getHeaderHeight();
+					newMargTop     = -Math.abs(heightEm) + 'em';
+					btnOffset      = '0em';
 					animate();
 				} else {
-					toggle = true;
+					isToggled = true;
 					bodyClassToggle();
-					dims = getDims(toggle);
-					move = dims.y + 'em';
-					offset = -Math.abs(dims.y) + 'em';
-					marg = (parseFloat(dims.marg) + parseFloat(dims.y)).toFixed(2) + 'em';
+					heightEm       = getHeaderHeight();
+					newMargTop     = '0em';
+					btnOffset      = -Math.abs( heightEm ) + 'em';
 					animate();
 				}
 			}
@@ -90,22 +70,21 @@ var hb_header = (function() {
 				ease:"elastic.out(1,0.8)"
 			},
 			force3D:true,
-			onComplete:active,
+			onComplete:isAnimatingToggleFalse,
 		});
-			headerHide.to(".header", { y:move }, 0)
-					  .to(".headerToggle", { y:offset }, 0)
-					  .to(".welcome", { marginTop:marg }, 0)
+			headerHide.to(".header", { marginTop:newMargTop }, 0)
+					  .to(".headerToggle", { y:btnOffset }, 0)
 	}
 
 	// Do nothing if animation is already in progress
-	function active() {
-		idle = true;
+	function isAnimatingToggleFalse() {
+		isAnimating = false;
 	}
 
 	// Add a classname to the body element when header is active
 	function bodyClassToggle() {
 		let body = document.querySelector('body');
-		if (toggle) {
+		if ( isToggled ) {
 			body.classList.add("header_active");
 		} else {
 			body.classList.remove("header_active");
