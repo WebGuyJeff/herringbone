0de88d022d470add65cf5fb0e9e071772d98fcde
wordpressjeff <me@jeffersonreal.com>
2023-01-31 14:01:31 +0000

    add services hover animation

--- js/style.js
+++ js/style.js
@@ -1,105 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/css/frontend/fonts.scss":
-/*!*************************************!*\
-  !*** ./src/css/frontend/fonts.scss ***!
-  \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
-/***/ "./src/css/frontend/hb.scss":
-/*!**********************************!*\
-  !*** ./src/css/frontend/hb.scss ***!
-  \**********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
-/***/ "./src/css/frontend/landing-dev.scss":
-/*!*******************************************!*\
-  !*** ./src/css/frontend/landing-dev.scss ***!
-  \*******************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
-/***/ "./src/css/frontend/landing.scss":
-/*!***************************************!*\
-  !*** ./src/css/frontend/landing.scss ***!
-  \***************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-/*!**************************!*\
-  !*** ./src/css/style.js ***!
-  \**************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _frontend_hb_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./frontend/hb.scss */ "./src/css/frontend/hb.scss");
-/* harmony import */ var _frontend_fonts_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./frontend/fonts.scss */ "./src/css/frontend/fonts.scss");
-/* harmony import */ var _frontend_landing_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./frontend/landing.scss */ "./src/css/frontend/landing.scss");
-/* harmony import */ var _frontend_landing_dev_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./frontend/landing-dev.scss */ "./src/css/frontend/landing-dev.scss");
 /**
  * Webpack entry point.
  * 
@@ -110,8 +11,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
-})();
 
 /******/ })()
 ;
--- src/js/frontend/animate-rollby.js
+++ src/js/frontend/animate-rollby.js
@@ -10,13 +10,30 @@ const animateRollBy = () => {
 
 	gsap.registerPlugin( ScrollTrigger )
 	gsap.ticker.fps( 30 )
-
+	
 	window.onload = () => {
-		
+	
 		const target    = document.querySelector( '#svg_tumbleweed' )
 		const cards     = document.querySelectorAll( '.lilCards_card' )
 		const container = target.parentElement
-
+	
+		// Setup hover effect with GSAP to avoid CSS conflict.
+		const hoverIn = ( e ) => gsap.to( e.target, { scale: 1.05, duration: 0.15 } )
+		const hoverOut = ( e ) => gsap.to( e.target, { scale: 1, duration: 0.15 } )
+		const addListeners = () => {
+			cards.forEach( ( card ) => {
+				card.addEventListener( 'mouseover', hoverIn )
+				card.addEventListener( 'mouseleave', hoverOut )
+			} )
+		}
+		const removeListeners = () => {
+			cards.forEach( ( card ) => {
+				card.removeEventListener( 'mouseover', hoverIn )
+				card.removeEventListener( 'mouseleave', hoverOut )
+			} )
+		}
+		addListeners()
+	
 		const getRollTime = () => {
 			const w = window.innerWidth
 			if ( w < 768 ) return 1
@@ -30,23 +47,25 @@ const animateRollBy = () => {
 			const rotation      = () => distance() / circumference * 360
 			const tl = gsap.timeline( { defaults: { ease: "none", duration: getRollTime() } } )
 				tl.fromTo( target, { x: 0 }, { x: distance() }, 0 )
-				.fromTo( target, { rotation: -rotation() }, { rotation: 0 }, 0 )
+				  .fromTo( target, { rotation: -rotation() }, { rotation: 0 }, 0 )
 			return tl
 		}
-
+	
 		const getSpinCardsTween = () => {
 			const getSpin = ( card ) => {
 				const tl = gsap.timeline( { repeat: 1 } )
 				tl.fromTo( card, { rotateY: 0 }, { rotateY: 360, duration: 0.8, ease: "none" } )
-				.fromTo( card, { background: '#fff' }, { background: '#e9e9e9', duration: 0.2 }, 0 )
-					.to( card, { background: '#fff', duration: 0.2 }, 0.2 )
-					.to( card, { background: '#e9e9e9', duration: 0.2 }, 0.4 )
-					.to( card, { background: '#fff', duration: 0.2 }, 0.6 )
+				  .fromTo( card, { background: '#fff', rotateX: 0 }, { background: '#e9e9e9', rotateX: 6, duration: 0.2 }, 0 )
+					  .to( card, { background: '#fff', rotateX: 10,duration: 0.2 }, 0.2 )
+					  .to( card, { background: '#e9e9e9', rotateX: 6,duration: 0.2 }, 0.4 )
+					  .to( card, { background: '#fff', rotateX: 0, duration: 0.2, clearProps: 'transform' }, 0.6 )
 				return tl
 			}
 			const getAllSpins = () => {
 				const tl    = gsap.timeline()
-				cards.forEach( ( card ) => tl.add( getSpin( card ), Math.random() / 2 ) ) // Random delay between 0 - 0.5.
+				cards.forEach( ( card ) => {
+					tl.add( getSpin( card ), Math.random() / 2 ) // Random delay between 0 - 0.5.
+				} )
 				return tl
 			}
 			const spinCards = () => {
@@ -56,7 +75,7 @@ const animateRollBy = () => {
 			}
 			return spinCards
 		}
-
+	
 		const newMaster = () => {
 			const master = gsap.timeline( {
 				defaults: { force3D:true },
@@ -70,12 +89,14 @@ const animateRollBy = () => {
 					// start: [trigger element position] [viewport position].
 					start: 'top center'
 				} } )
+				.call( removeListeners, null, 0 )
 				.add( getRollWeedTween() )
 				.add( getSpinCardsTween(), getRollTime() * .33 )
+				.call( addListeners, null, '+=0.5' )
 			return master
 		}
 		let master = newMaster()
-
+	
 		window.addEventListener( 'resize', () => {
 			master.kill()
 			master = newMaster()
